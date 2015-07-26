c512330922cbb234a21fbc217fd0cc115a7e4009
Sun <sun-shawn16@163.com>
2015-07-26 10:24:23 +0800

    fizzbuzz basic number print

---
+++ src/FizzBuzz.java
@@ -0,0 +1,22 @@
+
+public class FizzBuzz{
+    int n;
+    public FizzBuzz(){
+        n=100;
+    }
+    public FizzBuzz(int temp ){
+        n = temp;
+    }
+
+    public void play(){
+
+        for(int i =1 ;i <= n;i ++)
+        {
+            System.out.println( i );
+
+        }
+    }
+
+
+
+}
---
+++ test/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class FizzBuzzTest{
+    @Test
+    public void should_get_number_100_when_play(){
+        FizzBuzz game2 =  new FizzBuzz();
+        game2.play();
+    }
+
+
+
+}
